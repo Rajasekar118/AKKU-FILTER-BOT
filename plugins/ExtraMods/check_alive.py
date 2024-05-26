3464ae7d3bf92b82c2c08c164c890cf68808e5c1
Rajasekar118 <112805630+Rajasekar118@users.noreply.github.com>
2024-05-26 08:26:09 +0100

    Create check_alive.py

---
+++ plugins/ExtraMods/check_alive.py
@@ -0,0 +1,18 @@
+import time
+import random
+from pyrogram import Client, filters
+
+CMD = ["/", "."]
+
+@Client.on_message(filters.command("alive", CMD))
+async def check_alive(_, message):
+    await message.reply_text("He's not dead, honey, he's right here. You don't have any love for me anymore. Oh great.. You're not like a waste of time. 😔 At least occasionally, just try one/start... 🙂")
+
+
+@Client.on_message(filters.command("ping", CMD))
+async def ping(_, message):
+    start_t = time.time()
+    rm = await message.reply_text("...")
+    end_t = time.time()
+    time_taken_s = (end_t - start_t) * 1000
+    await rm.edit(f"Pong!\n{time_taken_s:.3f} ms")
