a1b30333a4e8bd7b9de57d3f04e4153364216bcc
Rajasekar S <112805630+Rajasekar118@users.noreply.github.com>
2024-06-03 17:08:38 +0100

    Create cbb.py

---
+++ plugins/cbb.py
@@ -0,0 +1,26 @@
+from pyrogram import __version__
+from bot import Bot
+from config import OWNER_ID
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+
+@Bot.on_callback_query()
+async def cb_handler(client: Bot, query: CallbackQuery):
+    data = query.data
+    if data == "about":
+        await query.message.edit_text(
+            text = f"<b>\n○  ᴄʀᴇᴀᴛᴏʀ : <a href='https://t.me/Raja_sekar_811'>Rajasekar</a>\n○  ʟᴀɴɢᴜᴀɢᴇ : <code>Eng Sub & Dub</code>\n○  Main Channel : <a href=https://t.me/Jitham_Movies>JITHAM_MOVIES</a>\n○  Update Channel : <a href=https://t.me/Akku_Bot_Official>AKKU_BOTZ</a>\n</b>",
+            disable_web_page_preview = True,
+            reply_markup = InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("🔒 Close", callback_data = "close")
+                    ]
+                ]
+            )
+        )
+    elif data == "close":
+        await query.message.delete()
+        try:
+            await query.message.reply_to_message.delete()
+        except:
+            pass
